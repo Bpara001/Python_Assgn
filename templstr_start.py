a102d3ce2a2a1914f6d4f2c01c3183eca7740ad7
Bpara001 <bpara001@ucr.edu>
2019-04-07 15:16:47 -0700

    -python language dictionary + templates complete

--- templstr_start.py
+++ templstr_start.py
@@ -1,18 +1,26 @@
 # demonstrate template string functions
 
-
+from string import Template
 def main():
+    instructor = "Joe Marini"
+    teaching = "Advanced Python"
     # Usual string formatting with format()
     str1 = "You're watching {0} by {1}".format("Advanced Python", "Joe Marini")
     print(str1)
     
     # TODO: create a template with placeholders
-    
-    # TODO: use the substitute method with keyword arguments
-    
-    # TODO: use the substitute method with a dictionary
+    t1= Template("You're watching ${classroom} by ${name}")
 
-    
+    # TODO: use the substitute method with keyword arguments
+    str2 = t1.substitute(classroom= teaching, name= instructor)
+    print(str2)
+    # TODO: use the substitute method with a dictionary
+    names = {
+        "name": instructor,
+        "classroom": teaching
+    }
+    strinDic= t1.substitute(names)
+    print(strinDic)
 if __name__ == "__main__":
     main()
     
