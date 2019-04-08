fae8241285850e1844267cb8c6f842f5d225f981
Bpara001 <bpara001@ucr.edu>
2019-04-07 21:10:18 -0700

    -Built-In Functions Completed

--- Python02/iterators_start.py
+++ Python02/iterators_start.py
@@ -5,17 +5,27 @@ def main():
     # define a list of days in English and French
     days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
     daysFr = ["Dim", "Lun", "Mar", "Mer", "Jeu", "Ven", "Sam"]
-
+    test = zip(days,daysFr)
+    testm= set(test)
+    print(test)
     # TODO: use iter to create an iterator over a collection
-
+    i = iter(days)
+    next(i)
+    # print(next(i))
     # TODO: iterate using a function and a sentinel
-
+    # with open('testfile.txt', 'r') as fp:
+    #   for block in iter(fp.readline,''):
+    #        print(block)
     # TODO: use regular interation over the days
-
+    # for m in range(len(days)):
+    #   print(m+1, days[m])
     # TODO: using enumerate reduces code and provides a counter
+    # for i,m in enumerate(days,start=1):
+    #   print(i,m[0] )
 
     # TODO: use zip to combine sequences
-
+    for i,m in enumerate(zip(days, daysFr), start=1):
+        print(i,m[0], "=", m[1], "in French")
 
 if __name__ == "__main__":
     main()
--- Python02/transforms_start.py
+++ Python02/transforms_start.py
@@ -2,19 +2,31 @@
 
 
 def filterFunc(x):
-    pass
+    if x % 2 == 0:
+        return False
+    return True
 
 
 def filterFunc2(x):
-    pass
+    if x.isupper():
+        return False
+    return True
 
 
 def squareFunc(x):
-    pass
+    return x*x
 
 
 def toGrade(x):
-    pass
+    if x >= 90:
+        return "A"
+    elif x >=80:
+        return "B"
+    elif x >= 70:
+        return "C"
+    elif x >= 65:
+        return "D"
+    return "F"
 
 
 def main():
@@ -24,13 +36,18 @@ def main():
     grades = (81, 89, 94, 78, 61, 66, 99, 74)
 
     # TODO: use filter to remove items from a list
-
+    odds = list(filter(filterFunc,nums))
+    print (odds)
     # TODO: use filter on non-numeric sequence
-
+    nonList = list(filter(filterFunc2,chars))
+    print (nonList)
     # TODO: use map to create a new sequence of values
-
+    square=list(map(squareFunc,nums))
+    print(square)
     # TODO: use sorted and map to change numbers to grades
-
+    grades = sorted(grades, reverse = True)
+    sorGrades = list(map(toGrade,grades))
+    print(sorGrades)
 
 if __name__ == "__main__":
     main()
